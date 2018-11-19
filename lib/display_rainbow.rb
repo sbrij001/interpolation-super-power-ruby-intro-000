18907b372d0269511dc1ac7cb7c39b191317a5ff
shivani brijmohan <sbrij001@yahoo.com>
2018-11-19 23:13:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,8 +6,6 @@
 # 5. Run `learn` locally until you pass.
 # 6. Submit the lab.
 
-
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  "R: #{[0]}, O: #{[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
+def display_rainbow()
 
 end
